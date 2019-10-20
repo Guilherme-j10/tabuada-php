fe61ac4fa77f845264935a690cc75087aecf3470
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2019-10-20 01:11:43 -0300

    encurtamento de código

--- index.php
+++ index.php
@@ -6,59 +6,15 @@
         <style>
             *{margin: 0px; padding: 0px; box-sizing: border-box; outline: none;}
 
-            body{
-                background-color: #333;
-            }
+            body{background-color: #333;}
 
-            .global{
-                width: 100%;
-                min-height: 100vh;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-
-            .global form{
-                background-color: #fff;
-                padding: 1.5%;
-                border-radius: 5px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
-            }
-
-            .global form label, input{
-                width: 100%;
-            }
-
-            .global form label {
-                font-family: Arial;
-
-            }
-
-            .global form  input{
-                padding: 12px;
-                border: none;
-                border-bottom: solid 1px #ccc;
-                margin: 10px;
-            }
-
-            .global form  button{
-                padding: 12px;
-                border: none;
-                cursor: pointer;
-                width: 100%;
-                border-radius: 50px;
-                background-color: #222;
-                color: #fff;
-                margin-bottom: 20px;
-                font-size: 18px;
-            }
-
-            .global form  p{
-                font-family: Arial;
-            }
+            .global{width: 100%; min-height: 100vh; display: flex; justify-content: center; align-items: center;}
+            .global form{background-color: #fff; padding: 1.5%; border-radius: 5px; display: flex; justify-content: center; align-items: center; flex-direction: column;}
+            .global form label, input{width: 100%;}
+            .global form label {font-family: Arial;}
+            .global form  input{padding: 12px; border: none; border-bottom: solid 1px #ccc; margin: 10px;}
+            .global form  button{padding: 12px; border: none; cursor: pointer; width: 100%; border-radius: 50px; background-color: #222; color: #fff; margin-bottom: 20px; font-size: 18px;}
+            .global form  p{font-family: Arial;}
         </style>
     </head>
     <body>
